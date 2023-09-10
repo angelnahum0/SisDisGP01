cd36dfa33f90879f9f5882b058883b1a63775179
angelnahum0 <49957626+angelnahum0@users.noreply.github.com>
2023-09-10 17:16:27 -0600

    Add files via upload

---
+++ codigojik.c
@@ -0,0 +1,103 @@
+#include <stdlib.h> 
+ 
+#include <stdio.h> 
+ 
+#include <sys/time.h> 
+#include <time.h> 
+ 
+ 
+void get_walltime (double* wcTime ) { 
+ 
+struct timeval tp ; 
+ 
+gettimeofday(&tp ,NULL) ; 
+ 
+*wcTime=(tp.tv_sec + tp.tv_usec/1000000.0) ; 
+ 
+} 
+ 
+ 
+ 
+int main (int argc, char* argv []) { 
+ 
+int i, j, k, n=10000,a; 
+ 
+int ** matrizA, **matrizB, **matrizC; 
+ 
+double S1, E1 ; 
+ 
+ 
+ 
+// I n i c i a l i z a n d o m a t ri c e s 
+ 
+matrizA = (int **)malloc(n*sizeof(int *)); 
+ 
+for ( i =0; i<n ; i++) 
+ 
+*(matrizA+i) = (int *) malloc(n*sizeof(int)); 
+ 
+ 
+matrizB = (int **)malloc(n*sizeof(int *)); 
+ 
+for ( i =0; i<n ; i++) 
+ 
+*(matrizB+i) = (int *) malloc(n*sizeof(int)); 
+ 
+ 
+matrizC = (int **)malloc(n*sizeof(int *)); 
+ 
+for ( i =0; i<n ; i++) 
+ 
+*(matrizC+i) = (int *) malloc(n*sizeof(int)); 
+//Llenando m a t ri c e s 
+ 
+for (i=0; i<n; ++i) 
+ 
+{ 
+ 
+for (j=0; j<n; ++j) 
+ 
+{ 
+ 
+matrizA[i][j] = rand( ) %6; 
+matrizB[i][j] = rand( ) %6; 
+ 
+} 
+ 
+} 
+ 
+printf("llenas matrices\n");
+ 
+ 
+get_walltime(&S1); 
+ 
+
+for (j=0; j<n; ++j) 
+ 
+{ 
+ 
+for (i = 0; i<n; ++i) 
+ 
+{ 
+ 
+for (k=0; k<n; ++k) 
+ 
+{ 
+ 
+matrizC[i][j]+= matrizA[i][k]*matrizB[k][j] ; 
+ 
+} 
+ 
+} 
+printf("valor de j: %d\n", j);
+} 
+ 
+get_walltime(&E1); 
+ 
+ 
+ 
+printf("Tiempo método jik: %f s\n", (E1-S1)) ; 
+ scanf("%d", &a);
+return 0; 
+ 
+}
